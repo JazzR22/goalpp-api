92ae59d123dd0a4343dd7ba94b8299310ef46d55
Jazmin S.R. <rudolphjazmin@gmail.com>
2025-04-13 23:16:03 -0300

    feat: goal routes

---
+++ routes/goal.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+const goalController = require('../controllers/goalController');
+
+// All routes below are protected
+router.use(auth);
+
+router.post('/', goalController.createGoal);
+router.get('/all', goalController.getUserGoals);
+router.delete('/:id', goalController.deleteGoal);
+router.put('/:id/day', goalController.toggleDay);
+router.get('/:id/percentage', goalController.calculatePercentage);
+router.put('/:id/updateGoal', goalController.extendGoalRange);
+
+module.exports = router;
